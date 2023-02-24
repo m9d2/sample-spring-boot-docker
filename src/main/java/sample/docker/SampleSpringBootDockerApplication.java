5c2753dc549749e73c48e61135f871b16dae25fe
gaoy <158612783@qq.com>
2023-02-24 13:29:31 +0800

    first commit

---
+++ src/main/java/sample/docker/SampleSpringBootDockerApplication.java
@@ -0,0 +1,13 @@
+package sample.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBootDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sample/docker/SampleSpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
